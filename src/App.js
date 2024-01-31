19c1d95c4a5b5b570f35dead8e3db60cc36dc7eb
Emory72 <hatusan1999@gmail.com>
2024-01-31 16:14:30 +0700

    reload main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import NotFound from "./components/NotFound/NotFound";
+import Signin from "./pages/Auth/pages/Signin/Signin";
+import Signup from "./pages/Auth/pages/Signup/Signup";
+import UserInfo from "./pages/UserInfo/UserInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Homepage />} />
+
+        {/* Public routes */}
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+
+        {/* User information */}
+        <Route path="users" element={<UserInfo />} />
+      </Route>
+    </Routes>
   );
 }
 
