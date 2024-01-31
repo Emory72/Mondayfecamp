83dfcfdaaf287c3c764a974572cfe44cc0c2f35d
Emory72 <hatusan1999@gmail.com>
2024-01-31 21:46:38 +0700

    signin layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+
+import { Header } from "antd/es/layout/layout";
 
 export default function MainLayout() {
-  return <div>MainLayout</div>;
+  return (
+    <div>
+      {/* <Header /> */}
+
+      <Outlet />
+    </div>
+  );
 }
